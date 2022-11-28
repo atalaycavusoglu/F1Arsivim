5eb74e0b422d86e1edeea3d0d4aaf9f5097116fd
atalaycavusoglu <atalaycavusoglu1@gmail.com>
2022-11-28 09:45:27 +0300

    Initial Commit

---
+++ F1Arsivim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1Arsivim
+//
+//  Created by Atalay Çavuşoğlu on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
